7d175b9eb50d462ac0e6ff6d97244027c363c5be
Colby Garland <colbygarland@gmail.com>
2025-02-24 17:40:46 -0700

    updates

--- pages/api/linkedin.ts
+++ pages/api/linkedin.ts
@@ -4,9 +4,28 @@ type ResponseData = {
   message: string
 }
  
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).json({ message: 'Hello from Next.js!' })
+  const accessToken = req.body.accessToken
+  const restli = req.body.restli
+  const linkedin_version = req.body.linkedin_version
+
+  delete req.body.accessToken
+  delete req.body.restli
+  delete req.body.linkedin_version
+
+  const response = await fetch('https://api.linkedin.com/rest/posts', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'X-Restli-Protocol-Version': restli,
+      'LinkedIn-Version': linkedin_version,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req.body)
+  })
+
+  res.status(response.status)
 }
